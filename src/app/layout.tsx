549d611ebdfe1059f5694e61ca0773dc060ebdd5
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-25 12:39:27 +0530

    Changes
    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/custom/header/Header'
+import Footer from '@/components/custom/footer/Footer'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      
+        <Header/>
+        {children}
+        <Footer/>
+
+      </body>
     </html>
   );
 }
