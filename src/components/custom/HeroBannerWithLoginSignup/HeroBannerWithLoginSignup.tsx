c589bc9fb95bd5584ba0b4ec7f5f75e5e47dd3c9
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-26 15:27:57 +0530

    Hero Banner
    Hero banner Changes

--- src/components/custom/HeroBannerWithLoginSignup/HeroBannerWithLoginSignup.tsx
+++ src/components/custom/HeroBannerWithLoginSignup/HeroBannerWithLoginSignup.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const HeroBannerWithLoginSignup = () => {
   return (
-    <div className="w-full flex flex-row justify-between px-8 py-4">
-        <div className="flex">
-            test1
+    <div className="w-full min-h-[520px] flex flex-row justify-between px-8 py-4 bg-slate-50">
+        <div className="flex flex-col gap-4 justify-center">
+            <h1 className="text-5xl font-semibold max-w-[580px]">Sabse Best Financial Consultation & Service</h1>
+            <p className="capitalize max-w-[400px]">Optimize & streamline your finances with beneficial subscription plans.</p>
         </div>
         <div className="flex">
             test2
