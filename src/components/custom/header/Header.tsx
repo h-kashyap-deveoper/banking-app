9349bd4330b1fdd656e003573f29e3586c2a621f
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-26 12:18:49 +0530

    Changes
    Changes

--- src/components/custom/header/Header.tsx
+++ src/components/custom/header/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex flex-row justify-between mx-auto px-8 py-4">
+        <div className="">
+            Credit Samdhan
+        </div>
+        <div className="">
+            test
+        </div>
+        <div className="">
+            <Button>Get A Loan</Button>
+        </div>
+
+    </div>
   )
 }
 
