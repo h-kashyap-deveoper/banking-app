37ab21ffb31be90668e9d9c1535cd45519d11c38
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-26 14:57:44 +0530

    Changes
    Changes

--- src/components/custom/header/Header.tsx
+++ src/components/custom/header/Header.tsx
@@ -7,14 +7,14 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <div className="flex flex-row justify-between mx-auto px-8 py-4 sticky top-0 shadow-md">
-        <div className="flex flex-row gap-2 py-auto text-lg font-semibold">
+        <div className="flex flex-row gap-2 py-auto text-lg font-semibold xl:min-w-[300px]">
             <Image src="/logo.png" alt="Logo" width="60" height="20"/>
             <p className="flex mt-2">Credit Samdhan</p>
         </div>
         <div className="">
             <HeaderMenuItems/>
         </div>
-        <div>
+        <div className="xl:min-w-[300px] flex justify-end">
             <Button>Get A Loan</Button>
         </div>
 
