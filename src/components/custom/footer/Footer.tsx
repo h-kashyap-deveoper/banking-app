36e304f6f0513724e2b4701364e3c5e552a702cf
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-26 14:43:36 +0530

    Changes
    Changes

--- src/components/custom/footer/Footer.tsx
+++ src/components/custom/footer/Footer.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const Footer = () => {
   return (
-    <div className="flex flex-row justify-between mx-auto px-8 py-4 sticky top-0 shadow-md">
-        Footer
+    <div className="bg-slate-100 flex flex-row justify-between mx-auto px-8 py-4 sticky top-0">
+        <div>
+        <Image src="/logo.png" alt="Logo" height="40" width="200"/>
+        <p>test</p>
+        </div>
     </div>
   )
 }
