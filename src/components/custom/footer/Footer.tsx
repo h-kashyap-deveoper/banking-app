236997cdb589e36284f5bf1d7057c0b3377399ba
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-26 14:52:29 +0530

    Changes
    Changes

--- src/components/custom/footer/Footer.tsx
+++ src/components/custom/footer/Footer.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className="bg-slate-100 flex flex-row justify-between mx-auto px-8 py-4 sticky top-0">
-        <div>
-        <Image src="/logo.png" alt="Logo" height="40" width="200"/>
+    <div className="bg-slate-100 flex flex-row justify-between mx-auto px-8 py-16 sticky top-0">
+        <div className="flex flex-col gap-4">
+        <Image src="/logo.png" alt="Logo" height="40" width="140"/>
         <p>test</p>
         </div>
     </div>
--- src/components/custom/header/Header.tsx
+++ src/components/custom/header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import HeaderMenuItems from '@/components/custom/header/HeaderMenuItems'
+import Image from 'next/image'
+
 
 const Header = () => {
   return (
     <div className="flex flex-row justify-between mx-auto px-8 py-4 sticky top-0 shadow-md">
-        <div className="flex flex-col py-auto text-lg font-semibold">
-            Credit Samdhan
+        <div className="flex flex-row gap-2 py-auto text-lg font-semibold">
+            <Image src="/logo.png" alt="Logo" width="60" height="20"/>
+            <p className="flex mt-2">Credit Samdhan</p>
         </div>
         <div className="">
             <HeaderMenuItems/>
